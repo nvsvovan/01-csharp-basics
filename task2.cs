2958f697502f9c89b0784259a1ef8cf268d43fa4
nvsvovan <104920346+nvsvovan@users.noreply.github.com>
2022-07-20 17:28:28 +0300

    Add files via upload

--- task2.cs
+++ task2.cs
@@ -1,23 +1,20 @@
 ﻿using System;
-class Program3
+class Program2
 {
-    private const char Symbol = '*';
-    private const char EmptySymbol = ' ';
-    private const int DoublingLengthValue = 2;
-    private const int NumberFirstSymbol = 1;
+    private const char SymbolOfTriangle = '*';
+    private const int FirstNumber = 0;
     static void Main()
     {
         int UserNumber;
         Console.Write("Введите произвольное положительное число N и нажмите Enter" +
         Environment.NewLine + "N равно: ");
-
         while (!int.TryParse(Console.ReadLine(), out UserNumber) || UserNumber <= 0)
-
             Console.Write("Ошибка! Вы ввели 0, отрицательное или не числовое значение," +
             Environment.NewLine + "попробуйте ещё раз: ");
 
-        for (int countSymbol = NumberFirstSymbol; countSymbol <= UserNumber; countSymbol++)
+        string symbol = SymbolOfTriangle.ToString();
 
-            Console.WriteLine(new string(EmptySymbol, UserNumber - countSymbol) + new string(Symbol, countSymbol * DoublingLengthValue - NumberFirstSymbol));
+        for (int countSymbol = FirstNumber; countSymbol < UserNumber; countSymbol++)
+            Console.WriteLine(symbol.PadLeft(countSymbol + 1, SymbolOfTriangle));
     }
 }
